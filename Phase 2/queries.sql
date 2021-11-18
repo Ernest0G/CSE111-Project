5048a4bb8538f0137684512e818cb425a70f7b63
Ernesto <56931380+Ernest0G@users.noreply.github.com>
2021-11-17 22:04:31 -0800

    added queries

--- Phase 2/queries.sql
+++ Phase 2/queries.sql
@@ -3,4 +3,63 @@
 -- g_phoneNumber = “724-529-8357”, g_email = “egutierrez99@gmail.com”, and g_guestCount = 1 
 
 INSERT INTO guest (g_guestNumber,g_name,g_phoneNumber,g_email,g_guestCount)
-VALUES (26, 'Ernesto Gutierrez', '724-529-8357', 'egutierrez99@gmail.com', 1)
+VALUES (26, 'Ernesto Gutierrez', '724-529-8357', 'egutierrez99@gmail.com', 1);
+
+-- 2)
+-- Show the room number, room capacity, bed count, and price for rooms that have a capacity of exactly 4. 
+
+SELECT r_roomNumber, r_roomCapacity, r_beds, r_pricePerNight
+FROM room
+WHERE r_roomCapacity = 4;
+
+-- 3)
+-- Find the total revenue for the day “2021-11-10”  
+SELECT SUM(rev_revenue)
+FROM revenue
+WHERE rev_date = '2021-11-10';
+
+-- 4)
+-- For each guest who bought a catering item, find the most expensive catering item they bought and 
+-- the catering type of it 
+SELECT cb_guestNumber,cb_foodName,c_type, MAX(cb_price)
+FROM cateringBill
+JOIN catering on c_name = cb_foodName
+GROUP BY cb_guestNumber;
+
+-- 5)
+-- Show all the rooms that have not been booked along with their capacity, price, and type.
+SELECT r_roomNumber, r_roomCapacity, r_pricePerNight, r_type
+FROM room
+WHERE r_roomNumber NOT IN 
+(
+    SELECT b_roomNumber as r_roomNumber
+    FROM booking
+);
+
+-- 6) 
+-- Find the catering item bought the most for each catering type
+SELECT cb_foodName, MAX(occurance) as, c_type
+FROM 
+(
+    SELECT cb_foodName, COUNT(cb_foodName) as occurance, c_type
+    FROM cateringBill
+    JOIN catering on c_name = cb_foodName
+    GROUP BY cb_foodName
+)
+GROUP BY c_type;
+
+-- 7)
+-- For each room that hasn’t been booked, find the rooms that have a capacity of 4 but only 2 beds. 
+-- Display the room number, number of beds, capacity, and price. 
+SELECT r_roomNumber, r_roomCapacity,r_beds, r_pricePerNight
+FROM
+(
+    SELECT r_roomNumber,r_beds, r_roomCapacity, r_pricePerNight, r_type
+    FROM room
+    WHERE r_roomNumber NOT IN 
+    (
+        SELECT b_roomNumber as r_roomNumber
+        FROM booking
+    )
+)
+WHERE r_roomCapacity = 4 AND r_beds = 2;
