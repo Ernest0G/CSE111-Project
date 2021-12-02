f3b852ec5adaec4bbf231447393155961b1304e5
Ernesto <ernesto7gutierrez@gmail.com>
2021-12-01 22:29:35 -0800

    started javascript to display content

---
+++ Hotel Management/hotel.js
@@ -0,0 +1,15 @@
+function displayView(tabName) {
+    var content;
+
+    content = document.getElementsByClassName("table-content");
+
+    for (var i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+
+    document.getElementById(pageName).style.display = "block"
+}
+
+function closeView(tabeName) {
+
+}
