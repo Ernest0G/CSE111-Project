4900e3531fb9860d9953119d04a54e8ab603e879
Ernesto <ernesto7gutierrez@gmail.com>
2021-12-03 15:37:33 -0800

    started routing

--- Hotel Management/server.py
+++ Hotel Management/server.py
@@ -1,15 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, jsonify
 import sqlite3
+from sqlite3 import Error
+from sqlite3.dbapi2 import Cursor
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print('landing')
     return render_template('index.html')
 
+@app.route('/viewRooms/showAllRooms', methods = ['GET'],endpoint = 'showAllRooms')
+def showStudentClasses():
+    try:
+        sql = """SELECT * FROM room GROUP BY r_roomNumber"""
+        Cursor.execute(sql)
+
+        rows = Cursor.fetchall()
+
+        print(rows)
+    except Error as e:
+        print(e)  
+    
+    data = []
+    for row in rows:
+        data.append(list(row))
+
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    con = sqlite3.connect('data.sqlite')
+    connection = sqlite3.connect("data.sqlite")
+
     app.run(host='localhost', port=5000, debug=True)
--- Hotel Management/static/javascript/hotel.js
+++ Hotel Management/static/javascript/hotel.js
@@ -19,3 +19,39 @@ function roomOptions(optionChosen) {
 
     }
 }
+
+function showAllRooms() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:5000/viewRooms/showAllRooms", true);
+    xhttp.send();
+
+    xhttp.onload = function () {
+        let rooms = {};
+        rooms = this.responseText;
+        console.log(rooms);
+        let roomsParsed = JSON.parse(rooms);
+
+        console.log(roomsParsed);
+
+        let t = '<tbody>'
+        t += '<tr>';
+        t += '<th>' + 'Room Number' + '</td>';
+        t += '<th>' + 'Bed Count' + '</td>';
+        t += '<th>' + 'Room Capacity' + '</td>';
+        t += '<th>' + 'Room Type' + '</td>';
+        t += '<th>' + 'Room Price (per night)' + '</td>';
+        t += '</tr>';
+        for (i in roomsParsed) {
+            t += '<tr>';
+            for (j in roomsParsed[i]) {
+                t += '<td>' + roomsParsed[i][j] + '</td>';
+            }
+            t += '</tr>';
+        }
+
+        t += '</table>'
+        t += '</tbody>';
+        document.getElementById('room-table').innerHTML = t;
+
+    };
+}
